896e3fdd5764a59aea538501c7d0cbe86932c2e0
tietnhathung <tietnhathung@gmail.com>
2023-05-21 11:38:26 +0700

    add Transactional LockByKey

---
+++ commons-library/src/main/java/com/example/commonslibrary/utils/LockByKey.java
@@ -0,0 +1,23 @@
+package com.example.commonslibrary.utils;
+
+import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class LockByKey {
+    private final Set<String> lockKeys = new HashSet<>();
+
+    public synchronized void lock(String key) throws InterruptedException {
+        while (lockKeys.contains(key)) {
+            wait();
+        }
+        lockKeys.add(key);
+    }
+
+    public synchronized void unlock(String key) {
+        lockKeys.remove(key);
+        notify();
+    }
+}
--- inventory-service/src/main/java/com/example/inventoryservice/InventoryService.java
+++ inventory-service/src/main/java/com/example/inventoryservice/InventoryService.java
@@ -1,8 +1,10 @@
 package com.example.inventoryservice;
 
 import com.example.commonslibrary.model.Inventory;
+import com.example.commonslibrary.utils.LockByKey;
 import com.example.inventoryservice.dao.InventoryEntity;
 import com.example.inventoryservice.dao.InventoryRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,9 +13,11 @@ import java.util.UUID;
 @Service
 public class InventoryService {
     private final InventoryRepository inventoryRepository;
+    private final LockByKey lockByKey;
 
-    public InventoryService(InventoryRepository inventoryRepository) {
+    public InventoryService(InventoryRepository inventoryRepository, LockByKey lockByKey) {
         this.inventoryRepository = inventoryRepository;
+        this.lockByKey = lockByKey;
     }
 
     public InventoryEntity findOrNew(UUID bookId){
@@ -34,7 +38,9 @@ public class InventoryService {
         return inventoryEntity.getQuantity() >= inventory.getQuantity();
     }
 
+    @Transactional
     public Inventory quantityAdjustment(Inventory inventory) throws Exception {
+        lockByKey.lock(inventory.getBookId().toString());
         InventoryEntity inventoryEntity = findOrNew(inventory.getBookId());
         int newQuantity = inventoryEntity.getQuantity() + inventory.getQuantity();
         if(newQuantity < 0){
@@ -42,6 +48,7 @@ public class InventoryService {
         }
         inventoryEntity.setQuantity( inventoryEntity.getQuantity() + inventory.getQuantity() );
         inventoryRepository.save(inventoryEntity);
+        lockByKey.unlock(inventory.getBookId().toString());
         return inventoryEntity.toModel();
     }
 }
